8afcec7e41ef51655f609862d9a1fb2aaaf3499f
Kristian Rincon <krisdaringon@gmail.com>
2025-01-03 18:35:28 -0500

    Refactor hamburger menu and integrating new Navbar component with mobile menu functionality

--- public/scripts/menu.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('flex');
-    document.querySelector('.nav-links').classList.toggle('hidden');
-  });
-  
